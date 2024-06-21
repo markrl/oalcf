ab4c709458ee95a97fc0a38deedad5738d5c9074
Mark Lindsey <markrlindsey@comcast.net>
2024-06-21 15:58:45 -0400

    score script update

--- baselines/ood/train_all_lid.sh
+++ baselines/ood/train_all_lid.sh
@@ -4,31 +4,31 @@ flags=$2
 echo $runname
 echo $flags
 
-rm -rf output/${runname}
-mkdir output/${runname}
+rm -rf baselines/ood/output/${runname}
+mkdir baselines/ood/output/${runname}
 rm -rf checkpoints/${runname}
 mkdir checkpoints/${runname}
 
 for lang in ha yo bas; do
     screen -dmS ${runname}_${lang} 
     screen -S ${runname}_${lang} -X stuff "source ~/oalcf3.11.7/bin/activate^M"
-    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=0 python train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
+    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=0 python baselines/ood/train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
 done
 
 for lang in ckb kmr sr; do
     screen -dmS ${runname}_${lang} 
     screen -S ${runname}_${lang} -X stuff "source ~/oalcf3.11.7/bin/activate^M"
-    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=1 python train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
+    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=1 python baselines/ood/train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
 done
 
 for lang in tt cv; do
     screen -dmS ${runname}_${lang} 
     screen -S ${runname}_${lang} -X stuff "source ~/oalcf3.11.7/bin/activate^M"
-    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=0 python train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
+    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=0 python baselines/ood/train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
 done
 
 for lang in ky hy-AM; do
     screen -dmS ${runname}_${lang} 
     screen -S ${runname}_${lang} -X stuff "source ~/oalcf3.11.7/bin/activate^M"
-    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=1 python train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
+    screen -S ${runname}_${lang} -X stuff "CUDA_VISIBLE_DEVICES=1 python baselines/ood/train_ood.py --ckpt_name ${runname}/${lang} --lid_target ${lang} --feat_root /data1/AfricanContinentLID/ecapalang/ ${flags}^M"
 done
---
+++ scoring/score_run.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+
+def main(score_path):
+    if 'scores.csv' not in score_path:
+        score_file = os.path.join(score_path, 'scores.csv')
+    else:
+        score_file = score_path
+    sheet = pd.read_csv(score_file)
+    dcf = np.array(sheet['cum_dcf'])[-1]
+    n_samples = np.array(sheet['n_samples'])[-1]
+    perc_targ = np.array(sheet['p_target'])[-1]*100
+    fps = np.sum(sheet['fps'])
+    fns = np.sum(sheet['fns'])
+    ns = np.sum(sheet['ns'])
+    ps = np.sum(sheet['ps'])
+    imlm = (fps+n_samples)/ns + fns/ps
+    fnr = fns/ps
+    fpr = fps/ns
+    p_samples = n_samples/(ns+ps+n_samples)
+    n_boot = sheet['n_samples'][0] - sheet['n_al'][0]
+    n_al = np.sum(sheet['n_al'])
+    n_cf_tp = np.sum(sheet['n_cf_tp'])
+    n_cf_fp = np.sum(sheet['n_cf_fp'])
+    print('DCF\tFNR\tFPR\tIMLM\t#bootstrap labels\t#AL labels\t#CF FP labels\t#CF TP labels\t% target in adapt pool\ttot labels/tot samples')
+    print(f'{dcf:.4f}\t{fnr:.4f}\t{fpr:.4f}\t{imlm:.4f}\t{n_boot:d}\t{n_al:d}\t{n_cf_fp:d}\t{n_cf_tp:d}\t{perc_targ:.2f}\t{p_samples:.2f}')
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        exit()
+    main(sys.argv[1])
