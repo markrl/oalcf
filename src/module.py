d21077ee3ff81db2c0a70cfcefc7f8e89487da3d
Mark Lindsey <mlindsey@PRBY-LPTP-100.probity.local>
2024-11-06 11:40:32 -0500

    Re-adding validation and logging for historic experiment

--- src/module.py
+++ src/module.py
@@ -45,6 +45,10 @@ class VtdModule(LightningModule):
         self.test_ns = 0
         self.test_ps = 0
         self.n_train = 0
+        self.test_scores = []
+        self.val_scores = []
+        self.test_labels = []
+        self.val_labels = []
         if params.cb_loss:
             self.n_target = 0
             self.n_nontarget = 0
@@ -79,11 +83,61 @@ class VtdModule(LightningModule):
         self.log('train/acc', acc, on_step=False, on_epoch=True)
         return loss
 
-    # def validation_step(self, batch, batch_idx):
-    #     return
+    def validation_step(self, batch, batch_idx):
+        if self.params.ensemble:
+            x,y,_,_ = batch
+            y_hat = self(x)
+            pred = torch.argmax(y_hat, dim=1)
+            acc = torch.mean(1.0*(pred==y))
+            self.log('val/acc', acc, on_step=False, on_epoch=True)
 
-    # def on_validation_epoch_end(self):
-    #     return
+            self.val_fps += torch.sum(torch.logical_and(y==0, pred==1))
+            self.val_fns += torch.sum(torch.logical_and(y==1, pred==0))
+            self.val_ps += torch.sum(y==1)
+            self.val_ns += torch.sum(y==0)
+            self.val_scores.append(y_hat[:,1])
+            self.val_labels.append(y)
+            return
+        x,y,_,_ = batch
+        embeds,y_hat,logits = self(x)
+        loss = self.criterion(y_hat,y)
+        if self.params.cb_loss:
+            loss = torch.mean(loss)
+        self.log('val/loss', loss.item(), on_step=False, on_epoch=True)
+        pred = torch.argmax(y_hat, dim=-1)
+        acc = torch.mean(1.0*(pred==y))
+        self.log('val/acc', acc, on_step=False, on_epoch=True)
+
+        self.val_fps += torch.sum(torch.logical_and(y==0, pred==1))
+        self.val_fns += torch.sum(torch.logical_and(y==1, pred==0))
+        self.val_ps += torch.sum(y==1)
+        self.val_ns += torch.sum(y==0)
+        self.val_scores.append(torch.exp(y_hat[:,1]))
+        self.val_labels.append(y)
+
+    def on_validation_epoch_end(self):
+        fnr = self.val_fns/self.val_ps if self.val_ps > 0 else 0.0
+        fpr = self.val_fps/self.val_ns if self.val_ns > 0 else 0.0
+        dcf = 0.25*fpr + 0.75*fnr
+        acc = 1-(self.val_fns+self.val_fps)/(self.val_ps+self.val_ns)
+        imlm = (self.val_fps+self.n_train)/(self.val_ns+self.val_ps) + fnr
+
+        self.log('val/fnr', fnr)
+        self.log('val/fpr', fpr)
+        self.log('val/dcf', dcf, prog_bar=True)
+        self.log('val/imlm', imlm)
+        self.log('val/acc', acc)
+        self.log('val/ps', float(self.val_ps))
+        self.log('val/fps', float(self.val_fps))
+        self.log('val/ns', float(self.val_ns))
+        self.log('val/fns', float(self.val_fns))
+
+        self.val_fns = 0
+        self.val_fps = 0
+        self.val_ps = 0
+        self.val_ns = 0
+        self.val_scores = []
+        self.val_labels = []
 
     def test_step(self, batch, batch_idx):
         if self.params.ensemble:
@@ -114,6 +168,8 @@ class VtdModule(LightningModule):
         self.test_fns += torch.sum(torch.logical_and(y==1, pred==0))
         self.test_ps += torch.sum(y==1)
         self.test_ns += torch.sum(y==0)
+        self.test_scores.append(torch.exp(y_hat[:,1]))
+        self.test_labels.append(y)
 
     def on_test_epoch_end(self):
         fnr = self.test_fns/self.test_ps if self.test_ps > 0 else 0.0
@@ -136,6 +192,8 @@ class VtdModule(LightningModule):
         self.test_fps = 0
         self.test_ps = 0
         self.test_ns = 0
+        self.test_scores = []
+        self.test_labels = []
 
     def configure_optimizers(self):
         if self.params.ensemble:
