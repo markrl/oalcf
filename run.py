9cf3195610a0b9c4549b7fcf29ecab45799aa2f3
Mark Lindsey <mlindsey@PRBY-LPTP-100.probity.local>
2024-12-11 23:47:20 -0500

    Update stopping threshold

--- run.py
+++ run.py
@@ -111,7 +111,8 @@ def main():
         monitor=params.monitor,
         mode=params.mode,
         patience=params.patience if params.patience_start is None else params.patience_start,
-        min_delta=params.min_delta
+        min_delta=params.min_delta,
+        stopping_threshold=0
     ))
     if params.pair_type=='offset':
         callbacks.append(IncOffsetCallback())
